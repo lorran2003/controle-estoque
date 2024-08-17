5d74f1b2db417c571c2d94a30a3fcd77991c10bc
VzinCoder <contato.viniciusp.b@gmail.com>
2024-08-17 00:56:08 -0300

    add product controller and validators to product controller

--- src/main/controller/productController.js
+++ src/main/controller/productController.js
@@ -1,16 +1,106 @@
 import db from "../database/db.js"
+import { createProductValidator, findByCodeValidator, findByIdValidator } from "../validator/productValidator.js"
+import CustomError from "../util/CustomError.js"
 
-const create = async (event,product) => {
-    try {
-      await db.Product.create(product)
-      return {error:false,msg:'Produto Criado com Sucesso'}
-    } catch (error) {
-      return {error:true,msg:'Erro ao criar o Produto'}
+
+const create = async (event, productData) => {
+  try {
+
+    const { error, value: productValid } = createProductValidator(productData)
+
+    if (error) {
+      throw new CustomError(error.message)
     }
+
+    const productCreated = await db.Product.create(productValid)
+
+    const response = {
+      error: false,
+      msg: 'Produto Criado com Sucesso',
+      data: productCreated.dataValues
+    }
+
+    return response
+  } catch (error) {
+
+    if (error instanceof CustomError) {
+      return { error: true, msg: error.message, data: null }
+    }
+
+    console.log(error)
+    return { error: true, msg: 'Erro Interno', data: null }
+  }
+}
+
+const findById = async (event, id) => {
+  try {
+    const { error, value: idValid } = findByIdValidator(id)
+
+    if (error) {
+      throw new CustomError(error.message)
+    }
+
+    const productFound = await db.Product.findByPk(idValid)
+
+    if (!productFound) {
+      throw new CustomError("Não existe Produto com esse id")
+    }
+
+    const response = {
+      error: false,
+      msg: "Produto encontrado",
+      data: productFound.dataValues
+    }
+
+    return response
+  } catch (error) {
+
+    if (error instanceof CustomError) {
+      return { error: true, msg: error.message, data: null }
+    }
+
+    console.log(error)
+    return { error: true, msg: 'Erro Interno', data: null }
+  }
+}
+
+const findByCode = async (event,code) => {
+  try {
+    const { error, value: codeValid } = findByCodeValidator(code)
+
+    if (error) {
+      throw new CustomError(error.message)
+    }
+
+    const productFound = await db.Product.findOne({where:{code:codeValid}})
+
+    if (!productFound) {
+      throw new CustomError("Não existe Produto com esse código")
+    }
+
+    const response = {
+      error: false,
+      msg: "Produto encontrado",
+      data: productFound.dataValues
+    }
+
+    return response
+  } catch (error) {
+
+    if (error instanceof CustomError) {
+      return { error: true, msg: error.message, data: null }
+    }
+
+    console.log(error)
+    return { error: true, msg: 'Erro Interno', data: null }
+  }
 }
 
 
 
+
 export default {
-    create,
+  create,
+  findById,
+  findByCode
 }
---
+++ src/main/validator/productValidator.js
@@ -0,0 +1,95 @@
+import Joi from "joi";
+
+
+const idValidation = Joi.number().integer().min(1).required().messages({
+    'integer.base':'O id deve ser um numero inteiro',
+    'any.required':'O id é obrigatório',
+    'number.min':'O valor deve ser maior que 0'
+})
+
+const nameValidation = Joi.string()
+    .min(1)
+    .max(255)
+    .required()
+    .messages({
+        'string.base': 'O nome deve ser uma string.',
+        'string.empty': 'O nome é obrigatório.',
+        'string.min': 'O nome deve ter pelo menos {#limit} caracteres.',
+        'string.max': 'O nome deve ter no máximo {#limit} caracteres.',
+        'any.required': 'O nome é obrigatório.'
+    })
+
+const codeValidation = Joi.string()
+    .length(6)
+    .pattern(/^[A-Z0-9]{6}$/)
+    .required()
+    .messages({
+        'string.base': 'O código deve ser uma string.',
+        'string.length': 'O código deve ter exatamente {#limit} caracteres.',
+        'string.pattern.base': 'O código deve conter apenas letras maiúsculas e números.',
+        'any.required': 'O código é obrigatório.'
+    })
+
+const imgValidation = Joi.string()
+    .allow(null)  
+    .optional()   
+    .messages({
+        'string.base': 'A imagem deve ser uma string.',
+        'any.allowNull': 'A imagem pode ser null.',
+    })
+
+
+const priceSaleValidation = Joi.number()
+    .integer()
+    .min(0)
+    .required()
+    .messages({
+        'number.base': 'O preço de venda deve ser um número.',
+        'number.integer': 'O preço de venda deve ser um número inteiro.',
+        'number.min': 'O preço de venda deve ser maior ou igual a {#limit}.',
+        'any.required': 'O preço de venda é obrigatório.'
+    })
+
+const priceCostValidation = Joi.number()
+    .integer()
+    .min(0)
+    .required()
+    .messages({
+        'number.base': 'O preço de custo deve ser um número.',
+        'number.integer': 'O preço de custo deve ser um número inteiro.',
+        'number.min': 'O preço de custo deve ser maior ou igual a {#limit}.',
+        'any.required': 'O preço de custo é obrigatório.'
+    })
+
+const stockValidation = Joi.number()
+    .integer()
+    .min(0)
+    .required()
+    .messages({
+        'number.base': 'O estoque deve ser um número.',
+        'number.integer': 'O estoque deve ser um número inteiro.',
+        'number.min': 'O estoque deve ser maior ou igual a {#limit}.',
+        'any.required': 'O estoque é obrigatório.'
+    })
+
+
+export const createProductValidator = (productData) => {
+   return Joi.object({
+        name:nameValidation,
+        code:codeValidation,
+        img:imgValidation,
+        priceSale:priceSaleValidation,
+        priceCost:priceCostValidation,
+        stock:stockValidation,
+    }).validate(productData)
+} 
+
+
+export const findByIdValidator = (id) => {
+    return idValidation.validate(id)
+}
+
+export const findByCodeValidator = (code)=> {
+    return codeValidation.validate(code)
+}
+
