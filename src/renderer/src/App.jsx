ee0a62996cd44748cb7dda9aedbd2e243ae22f9c
VzinCoder <contato.viniciusp.b@gmail.com>
2024-08-24 12:19:00 -0300

    change routes

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 export function App() {
   return (
     <main className="relative">
-      <Login />
       <Outlet />
     </main>
   )
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,29 +1,31 @@
-import './assets/main.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { Home } from './page/Home'
-import { CreateProduct } from './components/CreateProduct'
+import './assets/main.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { Home } from './page/Home';
+import { CreateProduct } from './components/CreateProduct';
 import {
-  createBrowserRouter,
+  createHashRouter,
   Route,
   createRoutesFromElements,
   RouterProvider
-} from 'react-router-dom'
-import { Product } from './page/Product'
+} from 'react-router-dom';
+import { Product } from './page/Product';
+import { Login } from './components/Login';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
+    <Route element={<App />}>
+      <Route path='/' element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/home/products" element={<Product />} />
       <Route path="/home/products/create-product" element={<CreateProduct />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
