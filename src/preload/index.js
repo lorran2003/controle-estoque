85a67a186f0d11b7fb28f6a838471fad18febfe8
VzinCoder <contato.viniciusp.b@gmail.com>
2024-09-17 18:50:38 -0300

    adjust api

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,15 +1,15 @@
-import { contextBridge,ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
+
 const productApi = {
-  create: (product) => ipcRenderer.invoke('create-product',product) ,
-  findById: (id) => ipcRenderer.invoke('findById-product',id),
-  findByCode:(code) => ipcRenderer.invoke('findByCode-product',code),
-  findByName:(name) => ipcRenderer.invoke('findByName-product',name),
-  delete:(id) => ipcRenderer.invoke('delete-product',id),
-  update:(product) => ipcRenderer.invoke('update-product',product),
-  findAll:() => ipcRenderer.invoke('findAll-product'),
+  create: (product) => ipcRenderer.invoke('create-product', product),
+  findById: (id) => ipcRenderer.invoke('findById-product', id),
+  findByCode: (code) => ipcRenderer.invoke('findByCode-product', code),
+  findByName: (name) => ipcRenderer.invoke('findByName-product', name),
+  delete: (id) => ipcRenderer.invoke('delete-product', id),
+  update: (product) => ipcRenderer.invoke('update-product', product),
+  findAll: () => ipcRenderer.invoke('findAll-product'),
 }
 
 // Use `contextBridge` APIs to expose Electron APIs to
@@ -24,5 +24,5 @@ if (process.contextIsolated) {
   }
 } else {
   window.electron = electronAPI
-  window.productApi = api
+  window.productApi = productApi;
 }
